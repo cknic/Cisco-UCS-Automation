e33e42f1136229d883c81259fa3b93f3cfe0d72f
Drew Russell <Drew.Russell@outlook.com>
2014-09-01 19:34:33 -0500

    Feature: Network Control Policy
    Added the ability to configure Network Control Policy(s) and verified
    that it works as intended in UCS Manager.

--- Cisco_UCS _Automation.py
+++ Cisco_UCS _Automation.py
@@ -1,6 +1,9 @@
 # Cisco UCS PowerShell Script Automation
 # Author: Drew Russell
 
+# -*- coding: UTF-8 -*-
+import os, sys
+
 
 # region Intro Text
 print ''
@@ -103,6 +106,8 @@ def has_numbers(input_string):
 #endregion
 
 #region VLAN Functions
+
+
 def vlan_configuration():
 # region VLAN Configuration
         print ''
@@ -138,19 +143,17 @@ def vlan_configuration():
         while option_vlan in ['no', 'n']:
                 break
 
-
-    
         #Prints PowerShell code to file depending on user input
         if vlan_type in ['global']:
             vlan_input()
             powershell_vlan_global()
-    
+
         elif vlan_type in ['fabric a']:
             print ''
             vlan_input()
             powershell_vlan_fabric('a')
             vlan_second_fabric('b')
-    
+
         elif vlan_type in ['fabric b']:
             print ''
             vlan_input()
@@ -173,11 +176,10 @@ def vlan_input():
             print ''
             vlan_count = raw_input('Enter the number of VLANs you would like to create: ')
 
-
     print ''
     vlan_name_list = []
     vlan_number_list = []
-    for user_vlan_data in range(1, (int(vlan_count)+1) ):
+    for user_vlan_data in range(1, (int(vlan_count)+1)):
         print 'VLAN %d Configuration: ' % user_vlan_data
         vlan_name = raw_input('Name: ')
 
@@ -199,7 +201,7 @@ def vlan_input():
             print '*** Error: VLAN Number(ID) is a mandatory field and may contain only numbers ***'
             print ''
             print 'VLAN %d Configuration: ' % user_vlan_data
-            print 'Name: ' +str(vlan_name)
+            print 'Name: ' + str(vlan_name)
             vlan_number = raw_input('Number (ID): ')
         #
 
@@ -209,7 +211,7 @@ def vlan_input():
 
 
 def vlan_second_fabric(c):
-    vlan_second_fabric_select = 'Would you like to configure VLANs for Fabric %s (yes/no): ' % ( str(c.upper()) )
+    vlan_second_fabric_select = 'Would you like to configure VLANs for Fabric %s (yes/no): ' % (str(c.upper()))
     option_vlan_second_fabric = raw_input(vlan_second_fabric_select).lower()
 
     while option_vlan_second_fabric not in ['yes', 'y', 'no', 'n']:
@@ -231,7 +233,7 @@ def powershell_vlan_global():
     global vlan_count
     global vlan_name_list
     global vlan_number_list
-    print 'VLAN Count = ' + str(vlan_count)
+
     for script_vlan_output in range(int(vlan_count)):
                     power_shell = open(project_name+'.ps1', "a")
                     power_shell.write(str('Get-UcsLanCloud | Add-UcsVlan -Name %s -Id %s' % (vlan_name_list[script_vlan_output], vlan_number_list[script_vlan_output])) + "\n")
@@ -248,8 +250,160 @@ def powershell_vlan_fabric(a):
 
 #endregion
 
-# region Create the PowerShell Script File
+# region Network Control Policy Functions
 
+
+def network_control_policy_configuration():
+# region Network Control Policy Configuration
+        print ''
+        option_network_control_policy = raw_input('Would you like to create a Network Control Policy (yes/no): ').lower()
+
+        while option_network_control_policy not in ['yes', 'y', 'no', 'n']:
+            print ''
+            print '*** Error: Please enter "Yes" or "No" ***'
+            print ''
+            option_network_control_policy = raw_input('Would you like to create a Network Control Policy : ').lower()
+
+        if option_network_control_policy in ['yes', 'y']:
+            print ''
+            print '**** Network Control Policy Configuration ****'
+            print ''
+            print ''
+            print 'The following options are available for each Network Control Policy: '
+            print ''
+            print ' * Cisco Discovery Policy (CDP)'
+            print ' * MAC Register Mode'
+            print ' * Action on Uplink Fail'
+            print ' * MAC Security'
+            print ''
+
+        while option_network_control_policy in ['no', 'n']:
+                break
+
+        network_control_policy_input()
+        powershell_network_control_policy()
+
+
+def network_control_policy_input():
+    global network_control_policy_name_list
+    global network_control_policy_cdp_list
+    global network_control_policy_mac_register_mode_list
+    global network_control_policy_uplink_fail_list
+    global network_control_policy_mac_security_list
+    #
+    global network_control_policy_count
+    global network_control_policy_name
+    global network_control_policy_cdp
+    global network_control_policy_uplink_fail
+    global network_control_policy_mac_security
+
+    network_control_policy_count = raw_input('Enter the number of unique Network Control Policies you would like to create: ')
+
+    #Verify that VLAN is populated and contains only numbers
+    while len(network_control_policy_count) < 1 or has_numbers(network_control_policy_count) is False:
+            print ''
+            print '*** Error: Please enter a valid number ***'
+            print ''
+            network_control_policy_count = raw_input('Enter the number of Network Control Policies you would like to create: ')
+
+
+    print ''
+    network_control_policy_name_list = []
+    network_control_policy_cdp_list = []
+    network_control_policy_mac_register_mode_list = []
+    network_control_policy_uplink_fail_list = []
+    network_control_policy_mac_security_list = []
+
+    for user_network_control_policy_data in range(1, (int(network_control_policy_count)+1) ):
+        print 'Network Control Policy %d Configuration: ' % user_network_control_policy_data
+        network_control_policy_name = raw_input('Name: ')
+
+        #Verify that the  Name field is populated and does not contain any numbers
+        has_numbers(network_control_policy_name)
+        while len(network_control_policy_name) < 1 or has_numbers(network_control_policy_name) is True:
+            print ''
+            print '*** Error: Network Control Policy Name is a mandatory field and may not contain numbers ***'
+            print ''
+            print 'Network Control Policy %d Configuration: ' % user_network_control_policy_data
+            network_control_policy_name = raw_input('Name: ')
+
+        #Verify that the  CDP field is populated with either enabled or disabled
+        print ''
+        print '*** CDP Default is Disabled ***'
+        network_control_policy_cdp = raw_input('Would you like CDP to be Enabled or Disabled? ').lower()
+
+        while network_control_policy_cdp not in ['enabled', 'disabled']:
+            print ''
+            print '*** Error: Please enter "Enabled" or "Disabled" ***'
+            print ''
+            network_control_policy_cdp = raw_input('Would you like CDP to be Enabled or Disabled? ').lower()
+
+
+        #Verify that the Mac Register Mode field is populated with either native or host
+        print ''
+        print '*** MAC Register Mode Default is Only Native Vlan ***'
+        network_control_policy_mac_register_mode = raw_input('Would you like the MAC Register Mode be set to "Only Native Vlan" or "All Host Vlan" (native/host) ').lower()
+
+        while network_control_policy_mac_register_mode not in ['native', 'host']:
+                print ''
+                print '*** Error: Please enter "Native" or "Host" ***'
+                print ''
+                network_control_policy_mac_register_mode = raw_input('Would you like the MAC Register Mode to be set to "Only Native Vlan" or "All Host Vlan" (native/host)? ').lower()
+
+        #Set the Network Control Policy to the correct PowerShell Formatting
+        if network_control_policy_mac_register_mode == "native":
+                network_control_policy_mac_register_mode = 'only-native-vlan'
+        elif network_control_policy_mac_register_mode == 'host':
+                network_control_policy_mac_register_mode = 'all-host-vlan'
+
+        #Verify that the  Action on Uplink Fail field is populated with link-down or warning
+        print ''
+        print '*** Action on Uplink Fail Default is Link Down ***'
+        network_control_policy_uplink_fail = raw_input('Would you like the Action on Uplink Fail be set to Link Down or Warning (down/warning)? ').lower()
+
+        while network_control_policy_uplink_fail not in ['down', 'warning']:
+            print ''
+            print '*** Error: Please enter "Down" or "Warning" ***'
+            print ''
+            network_control_policy_uplink_fail = raw_input('Would you like the Action on Uplink Fail be set to Link Down or Warning (down/warning)? ').lower()
+
+        #set the proper formatting for link down
+        if network_control_policy_uplink_fail == "down":
+                network_control_policy_uplink_fail = 'link-down'
+
+        #Verify that the  MAC Security field is populated with allow or deny
+        print ''
+        print '*** MAC Security Default is Allow ***'
+        network_control_policy_mac_security = raw_input('Would you like MAC Security to be set to Allow or Deny? ').lower()
+
+        while network_control_policy_mac_security not in ['allow', 'deny']:
+            print ''
+            print '*** Error: Please enter "Allow" or "Deny" ***'
+            print ''
+            network_control_policy_mac_security = raw_input('Would you like MAC Security to be set to Allow or Deny? ').lower()
+
+
+        print ''
+        network_control_policy_name_list.append(network_control_policy_name)
+        network_control_policy_cdp_list.append(network_control_policy_cdp)
+        network_control_policy_mac_register_mode_list.append(network_control_policy_mac_register_mode)
+        network_control_policy_uplink_fail_list.append(network_control_policy_uplink_fail)
+        network_control_policy_mac_security_list.append(network_control_policy_mac_security)
+
+
+def powershell_network_control_policy():
+    global power_shell
+    global network_control_policy_name_list
+    global network_control_policy_cdp_list
+    global network_control_policy_mac_register_mode_list
+    global network_control_policy_uplink_fail_list
+    global network_control_policy_mac_security_list
+    for script_network_control_policy_output in range(int(network_control_policy_count)):
+        power_shell = open(project_name+'.ps1', "a")
+        power_shell.write(str('$network_policy = Get-UcsOrg -Level root | Add-UcsNetworkControlPolicy -Name %s -Cdp %s -MacRegisterMode %s -UplinkFailAction %s | Set-UcsPortSecurityConfig -Forge %s' % (network_control_policy_name_list[script_network_control_policy_output], network_control_policy_cdp_list[script_network_control_policy_output], network_control_policy_mac_register_mode_list[script_network_control_policy_output], network_control_policy_uplink_fail_list[script_network_control_policy_output], network_control_policy_mac_security_list[script_network_control_policy_output])) + "\n")
+
+
+# endregion
 
 # endregion
 
@@ -272,8 +426,11 @@ elif len(project_name) >= 1:
 
 vlan_configuration()
 
+network_control_policy_configuration()
+
 
 powershell_final()
+
 # endregion
 
 
@@ -281,7 +438,7 @@ powershell_final()
 print ''
 print '********'
 print ''
-print 'The PowerShell Script %s.ps1 has been successfully exported to your root directory!' % project_name
+print 'The PowerShell Script %s.ps1 has been successfully exported to your current directory!' % project_name
 print ''
 print '********'
 # endregion
--- Dev.py
+++ Dev.py
@@ -5,7 +5,7 @@ def network_control_policy_configuration():
 
         while option_network_control_policy not in ['yes', 'y', 'no', 'n']:
             print ''
-            print '*** Error: Please enter "Yes or "No" ***'
+            print '*** Error: Please enter "Yes" or "No" ***'
             print ''
             option_network_control_policy = raw_input('Would you like to create a Network Control Policy : ').lower()
 
@@ -25,17 +25,22 @@ def network_control_policy_configuration():
         while option_network_control_policy in ['no', 'n']:
                 break
 
+        network_control_policy_input()
+        powershell_network_control_policy()
+
 def network_control_policy_input():
-    global network_control_policy_count
     global network_control_policy_name_list
+    global network_control_policy_cdp_list
     global network_control_policy_mac_register_mode_list
-    global network_control_uplink_fail_list
-    global network_control_mac_security_fail_list
+    global network_control_policy_uplink_fail_list
+    global network_control_policy_mac_security_list
+    #
+    global network_control_policy_count
     global network_control_policy_name
     global network_control_policy_cdp
-    global network_control_policy_mac_register_mode_list
-    global network_control_policy_up_link_fail
+    global network_control_policy_uplink_fail
     global network_control_policy_mac_security
+
     network_control_policy_count = raw_input('Enter the number of unique Network Control Policies you would like to create: ')
 
     #Verify that VLAN is populated and contains only numbers
@@ -47,11 +52,11 @@ def network_control_policy_input():
 
 
     print ''
-    network_control_policy_count = []
     network_control_policy_name_list = []
+    network_control_policy_cdp_list = []
     network_control_policy_mac_register_mode_list = []
-    network_control_uplink_fail_list = []
-    network_control_mac_security_fail_list = []
+    network_control_policy_uplink_fail_list = []
+    network_control_policy_mac_security_list = []
 
     for user_network_control_policy_data in range(1, (int(network_control_policy_count)+1) ):
         print 'Network Control Policy %d Configuration: ' % user_network_control_policy_data
@@ -66,26 +71,78 @@ def network_control_policy_input():
             print 'Network Control Policy %d Configuration: ' % user_network_control_policy_data
             network_control_policy_name = raw_input('Name: ')
 
-        network_control_policy_cdp = raw_input('Would you like to enable the Cisco Discovery Protocol (CDP) (yes/no): ').lower()
+        #Verify that the  CDP field is populated with either enabled or disabled
+        print ''
+        print '*** CDP Default is Disabled ***'
+        network_control_policy_cdp = raw_input('Would you like CDP to be Enabled or Disabled? ').lower()
 
-        while network_control_policy_cdp not in ['yes', 'y', 'no', 'n']:
+        while network_control_policy_cdp not in ['enabled', 'disabled']:
             print ''
-            print '*** Error: Please enter "Yes or "No" ***'
+            print '*** Error: Please enter "Enabled" or "Disabled" ***'
             print ''
-            network_control_policy_cdp = raw_input('Would you like to enable the Cisco Discovery Protocol (CDP): (yes/no) ').lower()
+            network_control_policy_cdp = raw_input('Would you like CDP to be Enabled or Disabled? ').lower()
 
 
-        #
+        #Verify that the Mac Register Mode field is populated with either native or host
+        print ''
+        print '*** MAC Register Mode Default is Only Native Vlan ***'
+        network_control_policy_mac_register_mode = raw_input('Would you like the MAC Register Mode be set to "Only Native Vlan" or "All Host Vlan" (native/host) ').lower()
+
+        while network_control_policy_mac_register_mode not in ['native', 'host']:
+                print ''
+                print '*** Error: Please enter "Native" or "Host" ***'
+                print ''
+                network_control_policy_mac_register_mode = raw_input('Would you like the MAC Register Mode to be set to "Only Native Vlan" or "All Host Vlan" (native/host)? ').lower()
+
+        #Set the Network Control Policy to the correct PowerShell Formatting
+        if network_control_policy_mac_register_mode == "native":
+                network_control_policy_mac_register_mode = 'only-native-vlan'
+        elif network_control_policy_mac_register_mode == 'host':
+                network_control_policy_mac_register_mode = 'all-host-vlan'
+
+        #Verify that the  Action on Uplink Fail field is populated with link-down or warning
+        print ''
+        print '*** Action on Uplink Fail Default is Link Down ***'
+        network_control_policy_uplink_fail = raw_input('Would you like the Action on Uplink Fail be set to Link Down or Warning (down/warning)? ').lower()
+
+        while network_control_policy_uplink_fail not in ['down', 'warning']:
+            print ''
+            print '*** Error: Please enter "Down" or "Warning" ***'
+            print ''
+            network_control_policy_uplink_fail = raw_input('Would you like the Action on Uplink Fail be set to Link Down or Warning (down/warning)? ').lower()
+
+        #set the proper formatting for link down
+        if network_control_policy_uplink_fail == "down":
+                network_control_policy_uplink_fail = 'link-down'
+
+        #Verify that the  MAC Security field is populated with allow or deny
+        print ''
+        print '*** MAC Security Default is Allow ***'
+        network_control_policy_mac_security = raw_input('Would you like MAC Security to be set to Allow or Deny? ').lower()
+
+        while network_control_policy_mac_security not in ['allow', 'deny']:
+            print ''
+            print '*** Error: Please enter "Allow" or "Deny" ***'
+            print ''
+            network_control_policy_mac_security = raw_input('Would you like MAC Security to be set to Allow or Deny? ').lower()
+
 
         print ''
-        vlan_name_list.append(vlan_name)
-        vlan_number_list.append(vlan_number)
+        network_control_policy_name_list.append(network_control_policy_name)
+        network_control_policy_cdp_list.append(network_control_policy_cdp)
+        network_control_policy_mac_register_mode_list.append(network_control_policy_mac_register_mode)
+        network_control_policy_uplink_fail_list.append(network_control_policy_uplink_fail)
+        network_control_policy_mac_security_list.append(network_control_policy_mac_security)
 
-
-
-def has_numbers(input_string):
-    return any(char.isdigit() for char in input_string)
-
+def powershell_network_control_policy():
+    global network_control_policy_name_list
+    global network_control_policy_cdp_list
+    global network_control_policy_mac_register_mode_list
+    global network_control_policy_uplink_fail_list
+    global network_control_policy_mac_security_list
+    for script_network_control_policy_output in range(int(network_control_policy_count)):
+                    power_shell = open(project_name+'.ps1', "a")
+                    power_shell.write(str('%s -MacRegisterMode –UplinkFailAction %s | Set-UcsPortSecurityConfig -Forge %s' % (network_control_policy_name_list[script_network_control_policy_output], network_control_policy_cdp_list[script_network_control_policy_output], network_control_policy_mac_register_mode_list[script_network_control_policy_output], network_control_policy_uplink_fail_list[script_network_control_policy_output], network_control_policy_mac_security_list[script_network_control_policy_output])) + "\n")
 
 
 network_control_policy_configuration()
